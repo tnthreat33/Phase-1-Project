0751fa2fd0e89fcbfbf5da2565c0f2fd54f48eb1
Rachel Hamby <tnthreat33@gmail.com>
2022-12-30 13:43:00 -0500

    delay answer

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     //URL for the API for all Jokes in full text form
-    const jokeAPI = "https://v2.jokeapi.dev/joke/Any?format=txt";
+    const jokeAPI = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist,explicit";
 
     //add eventListener to Click for Today's Joke button
     const form = document.querySelector(".form");
     form.addEventListener('click', (e)=>{
         e.preventDefault();
        return joke();
+       return jokeAnswer();
     });
 
    //Fetching Joke for API to be added as button response 
 function joke (){
     fetch(jokeAPI)
-    .then(res => res.text())
+    .then(res => res.json())
     .then((joke) => {
         const p = document.querySelector(".joke_paragraph");
-        p.textContent = joke;
+        p.textContent = joke.setup;
+        const a = document.querySelector(".joke_answer");
+        setTimeout(()=> {return  a.textContent = joke.delivery}, 5000 )
     });
 }
-
-
 
 })
